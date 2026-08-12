57ed863be6b699cf9505fba24490431a2ee297cb
zhangfeng <908980328@qq.com>
2026-08-12 13:20:13 +0800

    Build no-template OCR host EXE locally

--- build_host.bat
+++ build_host.bat
@@ -4,9 +4,11 @@ cd /d "%~dp0"
 
 python -m pip install -r requirements-host.txt
 python -m pip install pyinstaller
-pyinstaller --noconfirm --clean --onefile --windowed --uac-admin --name MusicVMAutoVisual host_visual.py
+python -m py_compile host_controller.py
+if errorlevel 1 exit /b 1
+pyinstaller --noconfirm --clean --onefile --windowed --uac-admin --collect-all rapidocr --name MusicVMAutoNoTemplate host_controller.py
 
 echo.
 echo Build finished:
-echo %CD%\dist\MusicVMAutoVisual.exe
+echo %CD%\dist\MusicVMAutoNoTemplate.exe
 pause
