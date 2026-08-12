b0fa14ad3058fbd170c9e59c3ebd903bcfc653bd
zhangfeng <908980328@qq.com>
2026-08-12 10:20:10 +0800

    Trigger Windows EXE build

--- build_host.bat
+++ build_host.bat
@@ -3,4 +3,5 @@ chcp 65001 >nul
 python -m pip install -r requirements-host.txt
 python -m pip install pyinstaller
 pyinstaller --noconfirm --clean --onefile --windowed --name MusicVMAutoHostDemo host_demo.py
+REM GitHub Actions can build the same EXE automatically.
 pause
