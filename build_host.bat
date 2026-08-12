62c82c970dde50aa52677ebd31e7a24dfc262048
zhangfeng <908980328@qq.com>
2026-08-12 15:05:19 +0800

    Use v0.5.4 entry for local EXE build

--- build_host.bat
+++ build_host.bat
@@ -6,7 +6,9 @@ python -m pip install -r requirements-host.txt
 python -m pip install pyinstaller
 python -m py_compile host_controller.py
 if errorlevel 1 exit /b 1
-pyinstaller --noconfirm --clean --onefile --windowed --uac-admin --collect-all rapidocr --name MusicVMAutoNoTemplate host_controller.py
+python -m py_compile host_controller_v054.py
+if errorlevel 1 exit /b 1
+pyinstaller --noconfirm --clean --onefile --windowed --uac-admin --collect-all rapidocr --name MusicVMAutoNoTemplate host_controller_v054.py
 
 echo.
 echo Build finished:
