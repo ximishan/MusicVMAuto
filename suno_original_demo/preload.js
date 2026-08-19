b893ab0a0128c109df680dd511140155f90d48dc
zhangfeng <908980328@qq.com>
2026-08-19 11:26:24 +0800

    Expose verification state events

--- suno_original_demo/preload.js
+++ suno_original_demo/preload.js
@@ -6,4 +6,9 @@ contextBridge.exposeInMainWorld('demoApi', {
   submitOriginal: (payload) => ipcRenderer.invoke('original:submit', payload),
   refreshTask: (task) => ipcRenderer.invoke('task:refresh', task),
   openSong: (url) => ipcRenderer.invoke('song:open', url),
+  onVerificationState: (handler) => {
+    const listener = (_event, payload) => handler(payload);
+    ipcRenderer.on('verification:state', listener);
+    return () => ipcRenderer.removeListener('verification:state', listener);
+  },
 });
